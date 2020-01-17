851712ead8ebb192920bd0b25b8fd86ae3735a9a
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-17 18:40:21 +0000

    modified query to implement user input variables

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -87,10 +87,32 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  `;
+
+  // Check if a city has been passed in as an option
+  if (options.city){
+    queryParams.push(`%${options.city}%`);
+    queryString +=  `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+
+  // run the query with the user input variables
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 
